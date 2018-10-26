fe4248b68a0ae215091f264335b82d85d3e90605
hschmidtke <44058199+hschmidtke@users.noreply.github.com>
2018-10-26 15:26:33 +0200

    Added store functions for debugging

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -2,7 +2,14 @@
 
 import { createStore } from "redux";
 import rootReducer from "../reducers/index";
+import { addArticle } from "../actions/index";
 
 const store = createStore(rootReducer);
+
+console.log(store.getState());
+
+store.subscribe(() => console.log('Look ma, Redux!!'))
+
+store.dispatch(addArticle({ name: 'React Redux Tutorial for Beginners', id: 1 }))
 
 export default store;
